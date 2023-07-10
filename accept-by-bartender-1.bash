245b026219297ea1e4ce717523a4f895cce71fcb
pjames <pjames@gmail.com>
2023-07-10 10:27:17 +0700

    Added subscription script with json format

--- accept-by-bartender-1.bash
+++ accept-by-bartender-1.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SUBSCRIPTION=drink-1-bartender
-gcloud pubsub subscriptions pull ${SUBSCRIPTION} --project="devops-skills-sharing"
+gcloud pubsub subscriptions pull ${SUBSCRIPTION} --project="devops-skills-sharing" --format=json | jq -r '.[].message.data | @base64d'

5ede9cf85f6a6e3d6384661b167a491724cadf76
pjames <pjames@gmail.com>
2023-07-10 10:41:48 +0700

    Added bartender while loop

--- accept-by-bartender-2.bash
+++ accept-by-bartender-2.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 SUBSCRIPTION=drink-1-bartender
-gcloud pubsub subscriptions pull ${SUBSCRIPTION} --auto-ack --project="devops-skills-sharing" --format=json | jq -r '.[].message.data | @base64d'
+while :
+do
+    gcloud pubsub subscriptions pull ${SUBSCRIPTION} --auto-ack --project="devops-skills-sharing" --format=json | jq -r '.[].message.data | @base64d'
+done

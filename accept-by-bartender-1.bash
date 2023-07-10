12ab4a1e633c363b1ee42e060d0b58c9f9b473c6
pjames <pjames@gmail.com>
2023-07-10 10:28:41 +0700

    Added --auto-ack

--- accept-by-bartender-1.bash
+++ accept-by-bartender-1.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SUBSCRIPTION=drink-1-bartender
-gcloud pubsub subscriptions pull ${SUBSCRIPTION} --project="devops-skills-sharing" --format=json | jq -r '.[].message.data | @base64d'
+gcloud pubsub subscriptions pull ${SUBSCRIPTION} --auto-ack --project="devops-skills-sharing" --format=json | jq -r '.[].message.data | @base64d'
